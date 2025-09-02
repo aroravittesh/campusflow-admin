90a8e7dd5bebdb9849e9858623159315da1b3af5
Khushi <khushi@Khushis-MacBook-Air.local>
2025-09-02 11:07:21 +0530

    sign-up page with image

--- src/app/admin/signup/[[...signup]]/page.tsx
+++ src/app/admin/signup/[[...signup]]/page.tsx
@@ -1,12 +1,12 @@
-import { SignUp } from "@clerk/nextjs";
+// import { SignUp } from "@clerk/nextjs";
 
-export default function AdminSignup() {
-  return (
-    <div className="flex h-screen items-center justify-center">
-      <SignUp />
-    </div>
-  );
-}
+// export default function AdminSignup() {
+//   return (
+//     <div className="flex h-screen items-center justify-center">
+//       <SignUp />
+//     </div>
+//   );
+// }
 // import { SignUp } from "@clerk/nextjs";
 
 // export default function AdminSignup() {
@@ -46,35 +46,39 @@ export default function AdminSignup() {
 //     </div>
 //   );
 // }
-// import { SignUp } from "@clerk/nextjs";
 
-// export default function AdminSignup() {
-//   return (
-//     <div className="flex h-screen">
-//       {/* Left side - Image only */}
-//       <div
-//         className="hidden lg:flex w-1/2 bg-cover bg-center"
-//         style={{
-//           backgroundImage:
-//             "url('https://images.unsplash.com/photo-1522071820081-009f0129c71c?auto=format&fit=crop&w=1500&q=80')", // replace with your photo
-//         }}
-//       />
+import { SignUp } from "@clerk/nextjs";
 
-//       {/* Right side - Welcome text + form */}
-//       <div className="flex w-full lg:w-1/2 items-center justify-center bg-white">
-//         <div className="w-full max-w-md p-8">
-//           <h1 className="text-4xl font-bold text-red-500 mb-6 text-center">
-//             Welcome Back!
-//           </h1>
-//           <SignUp
-//             appearance={{
-//               elements: {
-//                 card: "shadow-none border-none w-full", // make Clerk form clean
-//               },
-//             }}
-//           />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
+export default function AdminSignup() {
+  return (
+    <div className="flex h-screen">
+      
+<div className="hidden lg:flex w-1/2 overflow-hidden">
+  <img 
+    src="/image4.jpg" 
+    alt="Left panel" 
+    className="object-cover w-full h-full scale-110"
+  />
+  
+</div>
+
+
+
+      {/* Right side - Welcome text + form */}
+      <div className="flex w-full lg:w-1/2 items-center justify-center bg-white">
+        <div className="w-full max-w-md p-8">
+          <h1 className="text-4xl font-bold text-red-500 mb-6 text-center">
+            Welcome Back!
+          </h1>
+          <SignUp
+            appearance={{
+              elements: {
+                card: "shadow-none border-none w-full", // make Clerk form clean
+              },
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
